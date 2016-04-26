25d3d83237377cbdbe44720b5daa869b08eb1c42
Márton Papp <marton.papp@lensa.com>
2016-04-26 14:33:27 +0200

    fixed layout, game result form first steps

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Saga Node Client' });
 });
 
+router.get('/results', function(req, res, next) {
+  res.render('results', { title: 'Saga Node Client' });
+});
+
 router.post('/infosubmit', function(req, res, next) {
   const ajv = Ajv();
   const fields = req.body;
   const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-  
+
   // input validation
   const valid = ajv.validate(validator.schema, fields);
 
