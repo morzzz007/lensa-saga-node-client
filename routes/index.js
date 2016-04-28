6f623ef0ef5035cf67f1e7c3ea310813d56d6192
Márton Papp <morzzz007@gmail.com>
2016-04-28 12:47:28 +0200

    pre-filled sample clientId and apiKey

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const request = require('request');
 
 const STAGING_URL = 'https://pooledlabs.com/saga-api/make_game_request';
 const PRODUCTION_URL = 'https://lensa.com/saga-api/make_game_request';
-const CLIENTID = 1;
-const APIKEY = 2;
+const CLIENTID = '571dd4f12ecd8d002494b7b4';
+const APIKEY = 'zvScPOrGXpCD8Ny1nMgTr1DER4P+GplGEUSBbzvG1utY/wqH3fdNyg';
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Saga Node Client', clientId: CLIENTID, apiKey: APIKEY });
@@ -43,7 +43,7 @@ router.post('/infosubmit', function(req, res, next) {
       }
     }, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        res.redirect(response.redirect_url);
+        res.redirect(body.redirect_url);
       } else {
         res.render('validation', { message: JSON.stringify(body) });
       }
